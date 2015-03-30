0b5039ac92125427d0842412c0ba76404cc5f243
David Lai <me@dlai0001.com>
2015-03-29 23:11:07 -0700

    fix jslint issues

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var container = require('./src/Dependencies');
 
 
 
-module.exports = function (gridSize, difficulty, showSolution) {
-    var sudoku = new (container.get('Sudoku'));
+module.exports = function (gridSize, difficulty) {
+    var sudoku = new (container.get('Sudoku'))();
     var puzzle = sudoku.generate(gridSize, difficulty);
 
     return puzzle;
